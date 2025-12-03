0946df9bb394b583fb293e2f4f561d5d0b9251e0
Bisway111 <singhabis190@gmail.com>
2025-12-04 00:34:47 +0530

    updating yml

--- src/main/java/com/premierLeague/premier_Zone/config/RedisConfig.java
+++ src/main/java/com/premierLeague/premier_Zone/config/RedisConfig.java
@@ -10,6 +10,8 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
